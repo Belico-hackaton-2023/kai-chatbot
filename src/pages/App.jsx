815108567278defdd11b4513b070b547bd3f19d3
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-06-02 08:29:07 -0600

    fix(main):resolve conflicts

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,56 +1,8 @@
 import React, { useEffect, useContext } from 'react'
-import { ThemeProvider } from 'styled-components';
 import { UserContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 import ChatBotBox from '../pages/ChatBotBox'
-import theme from '../styles/thememui'
-import headKaiImg from '../content/images/kai-head.svg'
 
-
-// all available props
-const theme = {
-  background: '#f5f8fb',
-  fontFamily: 'Helvetica Neue',
-  headerBgColor: '#EF6C00',
-  headerFontColor: '#fff',
-  headerFontSize: '15px',
-  botBubbleColor: '#EF6C00',
-  botFontColor: '#fff',
-  userBubbleColor: '#fff',
-  userFontColor: '#4a4a4a',
-};
-
-
-const steps = [
-  {
-    id: '1',
-    message: 'Cuentame que quieres saber hoy?',
-    trigger: '2',
-  },
-  {
-    id: '2',
-    options: [
-      { value: 1, label: 'Glosario', trigger: '3' },
-      // { value: 2, label: 'Beneficios', trigger: '4' },
-      // { value: 3, label: 'Areas', trigger: '5' },
-    ],
-  },
-  {
-    id: '3',
-    message: 'Escribe alguna palabra que no sepas de kushki',
-    trigger: 'word',
-  },
-  {
-    id: 'word',
-    user: true,
-    trigger: 'definition',
-  },
-  {
-    id: 'definition',
-    component: <SearchGlosary />,
-    asMessage: true,
-  },
-];
 
 const App = () => {
   const navigate = useNavigate()
@@ -63,9 +15,7 @@ const App = () => {
   }, [userData]);
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <ChatBot steps={steps} botAvatar={headKaiImg} />
-      </ThemeProvider>
+      <ChatBotBox />
     </>
   )
 }
